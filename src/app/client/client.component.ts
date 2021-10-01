ca4ea0e54f8b060c651c3f268ff7d1b50b537fc0
thienth32 <64114903+thienth32@users.noreply.github.com>
2021-10-01 08:38:34 +0700

    get category list

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Category } from '../models/category';
+import {CategoryService} from '../services/category.service'
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
-
-  constructor() { }
+  categories: Category[] = [];
+  constructor(private cateService: CategoryService) { }
 
   ngOnInit(): void {
+    this.cateService.getList().subscribe(data => {
+      this.categories = data;
+      console.log(this.categories);
+    })
   }
 
 }
---
+++ src/app/services/category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { environment } from "../../environments/environment";
+import { HttpClient } from '@angular/common/http';
+import {Category} from '../models/category';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient) { }
+  getList(): Observable<Category[]>{
+    let listUrl = `${environment.apiUrl}/categories` ;
+    return this.http.get<Category[]>(listUrl);
+  }
+}
